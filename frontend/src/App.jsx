0eeddbf796adc943140ecc5e89d6b252897d1667
Okamoto Yuusuke <yuusuke_mbp@MacBook-Pro.localdomain>
2020-10-11 23:58:58 +0900

    Add Header and Footer w/ Bootstrap

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import {Container} from "react-bootstrap"
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Header />
+      <main className="py-3 t-3">
+        <Container>
+            <h1>Hello React-Commerce</h1>
+        </Container>
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
