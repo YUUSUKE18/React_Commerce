bf0cf0743c64439772ce42d17ef1f0c0e3f66986
Okamoto Yuusuke <yuusuke.o.work.1018@gmail.com>
2020-10-26 23:39:53 +0900

    Setting db

---
+++ backend/data/users.js
@@ -0,0 +1,22 @@
+import bcrypt from 'bcryptjs'
+
+const users = [
+    {
+        name: "Admin",
+        email: "admin@gmail.com",
+        password: bcrypt.hashSync("123456", 10),
+        isAdmin: true,
+    },
+    {
+        name: "user1",
+        email: "user1@gmail.com",
+        password: bcrypt.hashSync("123456", 10),
+    },
+    {
+        name: "user2",
+        email: "user2@gmail.com",
+        password: bcrypt.hashSync("123456", 10),
+    },
+]
+
+export default users;
